90c2dc491e330364327d72be2d849d6b63b940bf
KeshavBhagat09 <sendemailtokeshav@gmail.com>
2025-02-18 18:15:03 +0530

    Update TopPicks layout for responsive grid display

--- src/components/TopPicks/TopPicks.jsx
+++ src/components/TopPicks/TopPicks.jsx
@@ -43,7 +43,7 @@ const TopPicks = () => {
         )}
 
         {/* Movie Cards - Full Width */}
-        <div className="grid grid-cols-6 gap-4 w-full">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 w-full">
           {TopPicksCard.slice(startIndex, endIndex).map((movie, index) => (
             <MovieCard key={index} {...movie} />
           ))}
